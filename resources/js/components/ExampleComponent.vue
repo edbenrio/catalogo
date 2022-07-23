904ba108ffa4be70116fb68ada03d817b177df4e
hisakuma <hisakuma95@gmail.com>
2022-07-23 12:14:24 -0400

    merge hisakuma

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,92 +1,78 @@
 <template>
-<<<<<<< HEAD
-  <v-app id="inspire">
-    <v-app-bar
-      app
-      shrink-on-scroll
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app id="inspire">
+        <v-app-bar app shrink-on-scroll>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+            <v-toolbar-title>Application</v-toolbar-title>
 
-      <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
+            <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+        </v-app-bar>
 
-    <v-main>
-      <v-container>
-        <v-row>
-            <v-col
-                class="d-flex align-end"
-                cols="12"
-                sm="8"
-                md="8"
-            >
-                <v-file-input
-                    v-model="filename"
-                    accept="image/*"
-                    label="Selccionar Imagen"
-                    show-size
-                    counter
-                    clearable
-                    @change="onFileChange"
-                >
-                </v-file-input>
-            </v-col>
-            <v-col
-                class="d-flex align-end mb-5"
-                cols="12"
-                sm="4"
-                md="4"
-            >
-                <v-sheet elevation="2" class="rounded">
-                    <v-img
-                        v-if="showImg"
-                        v-bind:src="showImg"
-                        height="150"
-                        width="120"
-                        class="rounded text-center align-end pa-1"
-                        aspect-ratio="2"
-                        contain
+        <v-main>
+            <v-container>
+                <v-row>
+                    <v-col class="d-flex align-end" cols="12" sm="8" md="8">
+                        <v-file-input
+                            v-model="filename"
+                            accept="image/*"
+                            label="Selccionar Imagen"
+                            show-size
+                            counter
+                            clearable
+                            @change="onFileChange"
+                        >
+                        </v-file-input>
+                    </v-col>
+                    <v-col
+                        class="d-flex align-end mb-5"
+                        cols="12"
+                        sm="4"
+                        md="4"
                     >
-                        <v-tooltip bottom>
-                            <template
-                                v-slot:activator="{
-                                    on,
-                                    attrs
-                                }"
+                        <v-sheet elevation="2" class="rounded">
+                            <v-img
+                                v-if="showImg"
+                                v-bind:src="showImg"
+                                height="150"
+                                width="120"
+                                class="rounded text-center align-end pa-1"
+                                aspect-ratio="2"
+                                contain
                             >
-                                <v-fab-transition>
-                                    <v-btn
-                                        x-small
-                                        dark
-                                        bottom
-                                        color="red"
-                                        v-on="on"
-                                        v-bind="attrs"
-                                        @click="resetImage"
-                                        >Eliminar
-                                    </v-btn>
-                                </v-fab-transition>
-                            </template>
-                            <span>Eliminar Imagen</span>
-                        </v-tooltip>
-                    </v-img>
-                </v-sheet>
-            </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-  </v-app>
+                                <v-tooltip bottom>
+                                    <template v-slot:activator="{ on, attrs }">
+                                        <v-fab-transition>
+                                            <v-btn
+                                                x-small
+                                                dark
+                                                bottom
+                                                color="red"
+                                                v-on="on"
+                                                v-bind="attrs"
+                                                @click="resetImage"
+                                                >Eliminar
+                                            </v-btn>
+                                        </v-fab-transition>
+                                    </template>
+                                    <span>Eliminar Imagen</span>
+                                </v-tooltip>
+                            </v-img>
+                        </v-sheet>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-  export default {
-    methods:{
-		onFileChange(payload) {
+export default {
+    methods: {
+        onFileChange(payload) {
             const file = payload; //obtener datos de la variable
             if (file) {
                 this.showImg = URL.createObjectURL(file); //guardar solo la ruta guardada
@@ -96,26 +82,6 @@
                 this.setImage(null);
             }
         },
-	}
-  }
-</script>
-=======
-    <v-app id="inspire">
-        <navigation />
-        <v-main>
-            <v-container>
-                <router-view></router-view>
-            </v-container>
-        </v-main>
-    </v-app>
-</template>
-
-<script>
-import navigation from "../page/Navigation.Component.vue";
-export default {
-    components: {
-        navigation,
     },
 };
 </script>
->>>>>>> hisakuma
