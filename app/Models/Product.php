e7cc7e1bedb72608a54beda4c9ef9b0ff06b28de
Edgar Cano <edgarrubendario@gmail.com>
2022-07-25 16:02:08 -0400

    Corregir product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,7 +16,10 @@ class Product extends Model
         'nombre',
         'descripcion',
         'venta',
-        'alquiler'        
+        'alquiler',
+        'codigo',
+        'brand_id',
+        'category_id'        
     ];
 
     public function brand(){
