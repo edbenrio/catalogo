c77b6df6af8990101fab5044bb4156dbe5e4c007
hisakuma <hisakuma95@gmail.com>
2022-07-26 10:47:21 -0400

    push

--- resources/js/page/admin/Product/FormProduct.Component.vue
+++ resources/js/page/admin/Product/FormProduct.Component.vue
@@ -3,98 +3,144 @@
         <v-dialog v-model="dialog" @click:outside="setDialog">
             <v-card class="pa-5">
                 <v-card-title class="ml-n5"> Nuevo Producto </v-card-title>
-
-                <v-row>
-                    <v-col cols="6">
-                        <v-text-field
-                            v-model="product.nombre"
-                            placeholder="Nombre Producto"
-                            label="Nombre"
-                        ></v-text-field>
-                    </v-col>
-                    <v-col cols="6">
-                        <v-text-field
-                            v-model="product.codigo"
-                            placeholder="ej: 858478587"
-                            label="Codigo"
-                        ></v-text-field>
-                    </v-col>
-
-                    <v-col cols="12">
-                        <!-- seleccion y busqueda de marca -->
-                        <v-autocomplete
-                            :menu-props="{
-                                nudgeBottom: 15 + 'px',
-                            }"
-                            v-model="product.brand_id"
-                            :items="brands"
-                            :search-input.sync="searchBrand"
-                            :rules="marcaRules"
-                            label="Elija una marca"
-                            item-text="nombre"
-                            item-value="id"
-                            cache-items
-                            clearable
+                <v-expansion-panels
+                    v-model="panel"
+                    :disabled="disabled"
+                    multiple
+                >
+                    <v-expansion-panel>
+                        <v-expansion-panel-header color="grey lighten-4 "
+                            >Detalle Productos</v-expansion-panel-header
                         >
-                            <!-- En caso que no encuentra -->
-                            <!-- Opcion para crear marca -->
-                            <template v-slot:no-data>
-                                <v-sheet class="d-flex justify-center ma-2">
-                                    <h5>Marca inexistente</h5>
-                                </v-sheet>
-                                <v-sheet class="d-flex justify-center">
-                                    <v-btn
-                                        @click="
-                                            setDialogBrand();
-                                            setIsBrandForOther();
-                                        "
-                                        >Crear Marca</v-btn
-                                    >
-                                </v-sheet>
-                            </template>
+                        <v-expansion-panel-content>
+                            <v-row>
+                                <v-col cols="5">
+                                    <v-row>
+                                        <v-col cols="6">
+                                            <v-text-field
+                                                v-model="product.nombre"
+                                                placeholder="Nombre Producto"
+                                                label="Nombre"
+                                            ></v-text-field>
+                                        </v-col>
+                                        <v-col cols="6">
+                                            <v-text-field
+                                                v-model="product.codigo"
+                                                placeholder="ej: 858478587"
+                                                label="Codigo"
+                                            ></v-text-field>
+                                        </v-col>
 
-                            <template v-slot:item="{ item }">
-                                <v-img
-                                    max-height="50"
-                                    max-width="50"
-                                    :src="item.img_url"
-                                    class="rounded"
-                                ></v-img>
-                                {{ item.nombre }}
-                            </template>
-                        </v-autocomplete>
-                    </v-col>
+                                        <v-col cols="12">
+                                            <!-- seleccion y busqueda de marca -->
+                                            <v-autocomplete
+                                                :menu-props="{
+                                                    nudgeBottom: 15 + 'px',
+                                                }"
+                                                v-model="product.brand_id"
+                                                :items="brands"
+                                                :search-input.sync="searchBrand"
+                                                :rules="marcaRules"
+                                                label="Elija una marca"
+                                                item-text="nombre"
+                                                item-value="id"
+                                                cache-items
+                                                clearable
+                                            >
+                                                <!-- En caso que no encuentra -->
+                                                <!-- Opcion para crear marca -->
+                                                <template v-slot:no-data>
+                                                    <v-sheet
+                                                        class="d-flex justify-center ma-2"
+                                                    >
+                                                        <h5>
+                                                            Marca inexistente
+                                                        </h5>
+                                                    </v-sheet>
+                                                    <v-sheet
+                                                        class="d-flex justify-center"
+                                                    >
+                                                        <v-btn
+                                                            @click="
+                                                                setDialogBrand();
+                                                                setIsBrandForOther();
+                                                            "
+                                                            >Crear Marca</v-btn
+                                                        >
+                                                    </v-sheet>
+                                                </template>
 
-                    <v-col cols="12">
-                        <v-textarea
-                            v-model="product.descripcion"
-                            placeholder="Descripcion"
-                            label="Descripcion"
-                            rows="3"
-                        ></v-textarea>
-                    </v-col>
+                                                <template
+                                                    v-slot:item="{ item }"
+                                                >
+                                                    <v-img
+                                                        max-height="50"
+                                                        max-width="50"
+                                                        :src="item.img_url"
+                                                        class="rounded"
+                                                    ></v-img>
+                                                    {{ item.nombre }}
+                                                </template>
+                                            </v-autocomplete>
+                                        </v-col>
 
-                    <v-col cols="4">
-                        <v-subheader class="mr-2">Metodo </v-subheader>
-                        <v-row>
-                            <v-col cols="6">
-                                <v-checkbox
-                                    v-model="product.venta"
-                                    label="Venta"
-                                    :value="1"
-                                ></v-checkbox>
-                            </v-col>
-                            <v-col cols="6">
-                                <v-checkbox
-                                    v-model="product.alquiler"
-                                    label="Alquiler"
-                                    :value="1"
-                                ></v-checkbox>
-                            </v-col>
-                        </v-row>
-                    </v-col>
-                </v-row>
-
+                                        <v-col cols="12">
+                                            <v-textarea
+                                                v-model="product.descripcion"
+                                                placeholder="Descripcion"
+                                                label="Descripcion"
+                                                rows="5"
+                                            ></v-textarea>
+                                        </v-col>
+                                    </v-row>
+                                </v-col>
+                                <v-col cols="1">
+                                    <v-divider vertical></v-divider>
+                                </v-col>
+                                <v-col cols="5">
+                                    <v-col cols="12">
+                                        <v-subheader class="ml-n4"
+                                            >Metodo
+                                        </v-subheader>
+                                        <v-row class="mt-n5">
+                                            <v-col cols="6">
+                                                <v-checkbox
+                                                    v-model="product.venta"
+                                                    label="Venta"
+                                                    :value="1"
+                                                ></v-checkbox>
+                                            </v-col>
+                                            <v-col cols="6">
+                                                <v-checkbox
+                                                    v-model="product.alquiler"
+                                                    label="Alquiler"
+                                                    :value="1"
+                                                ></v-checkbox>
+                                            </v-col>
+                                        </v-row>
+                                    </v-col>
+                                    <v-col cols="12" class="mt-n5">
+                                        <v-subheader class="ml-n4"
+                                            >Agregar Imagen</v-subheader
+                                        >
+                                        <upload-media
+                                            class="scroll-media"
+                                            server="/image"
+                                            ref="getImages"
+                                        >
+                                        </upload-media>
+                                    </v-col>
+                                </v-col>
+                            </v-row>
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                    <v-expansion-panel>
+                        <v-expansion-panel-header color="grey lighten-4 "
+                            >Detalle Adicionales</v-expansion-panel-header
+                        >
+                        <v-expansion-panel-content> </v-expansion-panel-content>
+                    </v-expansion-panel>
+                </v-expansion-panels>
                 <v-card-actions>
                     <v-spacer></v-spacer>
                     <v-btn color="blue darken-1" text @click="setDialog">
@@ -115,6 +161,8 @@
 
 <script>
 import { mapMutations, mapState, mapActions } from "vuex";
+import { UploadMedia, UpdateMedia } from "vue-media-upload";
+
 export default {
     data: () => ({
         valid: true,
@@ -126,6 +174,10 @@ export default {
         filename: null,
         showImg: null,
     }),
+    components: {
+        UploadMedia,
+        UpdateMedia,
+    },
     watch: {
         //actualiza al agregar nueva Marca desde el Form Producto
         //agrega el id Marca creado al campo de seleccion Marca
@@ -188,16 +240,13 @@ export default {
         //Dependiendo del edit (boolean), crea o edita la marca
         //resetea validacion, obtiene las listas marca, borra los campos nombre url
         validateSubmit(edit, item) {
-            this.product.img_url = this.img;
-            console.log(item);
-
             if (this.$refs.form.validate()) {
                 if (edit) {
                     this.editProduct(item);
                 } else {
                     this.createProduct(item);
+                    this.guardarImagen();
                 }
-
                 this.resetValidate();
                 this.clearBrandForOther();
             }
@@ -205,8 +254,17 @@ export default {
         resetValidate() {
             this.$refs.form.resetValidation();
         },
+        guardarImagen() {
+            this.$refs.getImages.media;
+        },
     },
 };
 </script>
 
-<style></style>
+<style scoped>
+.scroll-media {
+    height: 300px;
+    overflow: hidden;
+    overflow-y: scroll;
+}
+</style>
