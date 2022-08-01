299de3190a519f62282cd595c3c36f98623ec688
Edgar Cano <edgarrubendario@gmail.com>
2022-08-01 13:52:29 -0400

    Corregir ortografía

--- resources/js/page/PageNotFound.Component.vue
+++ resources/js/page/PageNotFound.Component.vue
@@ -5,7 +5,7 @@
                 <h1 class="display-1 fw-bold">404</h1>
                 <p class="fs-3"> <span class="text-danger">¡Ups!</span> Página no encontrada.</p>
                 <p class="lead">
-                    La página a la que quieres ingresar no existe o fue reomvida
+                    La página a la que quieres ingresar no existe o fue removida
                   </p>
                 <a href="/home" class="btn btn-primary">Ir al inicio</a>
             </div>
