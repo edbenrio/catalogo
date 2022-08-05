28705ab03277d4386c139d686645edfa59cf39d9
Edgar Cano <edgarrubendario@gmail.com>
2022-08-05 10:36:38 -0400

    Agregar Metadatos a los productos

--- resources/views/showProduct.blade.php
+++ resources/views/showProduct.blade.php
@@ -3,6 +3,17 @@
     <meta property="og:url" content="DA QD">
     <meta property="og:image" content="holaaa">
     <meta property="og:description" content="{{ $product->descripcion }}"/>
+
+    <meta name="og:site_name" property="og:site_name" content="Itapua Medical, Oxigeno medicinal">
+    <meta name="title" content="{{$product->nombre}}">
+    <meta name="description" content="{{$product->descripcion}}">
+    <meta property="og:title" content="{{$product->nombre}}">
+    <meta property="og:image" content="{{count($product->image)>0? '$product->image[0]->img_url' : '' }}">
+    <meta property="og:description" content="{{$product->descripcion}}">
+    <meta property="og:image_link" content="{{count($product->image)>0? '$product->image[0]->img_url' : '' }}">
+    <meta property="og:url" content="/products/{{$product->id}}">
+    <meta data-n-property="og:image:alt" content="{{$product->nombre}}">
+
     <title>{{$product->nombre}}</title>
 @endsection
 @section('content')    
